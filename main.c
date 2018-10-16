38a463326dd5593e230699ca365edccb0800a7a1
김영근 <yungdai0@gmail.com>
2018-10-16 16:03:21 +0900

    fixed #1 스켈레톤 작성.

---
+++ main.c
@@ -0,0 +1,25 @@
+int add(int, int);
+int sub(int, int);
+
+int main(int argc, char* argv[])
+{
+	int a, b, c;
+	char opertator;
+	
+	printf("Please enter a expression");
+	scanf("%d %c %d", &a, &operator, &b);
+	
+	switch (operator)
+	{
+		case '+':
+			c = add(a, b);
+			break;
+		case '-':
+			c = sub(a, b);
+			break;
+	}
+	printf("result:%d", c);
+	
+	return 0;
+}
+		
